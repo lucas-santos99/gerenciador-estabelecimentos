3ceacf51558657c02e91f747c5488c7f24c2e02d
lucas-santos99 <lucasluckas10@gmail.com>
2026-03-25 15:51:34 -0300

    ajuste novo superadmin 2

--- routes/superAdminRoutes.js
+++ routes/superAdminRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { verificarPermissao } = require('../middlewares/verificarPermissao');
 const { criarSuperAdmin } = require('../controllers/superAdminController');
+const authUser = require('../middlewares/authUser');
 
 // 🔥 só super admin acessa
 router.post(
   '/criar',
+  authUser, // 🔥 ESSENCIAL
   verificarPermissao('qualquer_coisa'),
   criarSuperAdmin
 );
